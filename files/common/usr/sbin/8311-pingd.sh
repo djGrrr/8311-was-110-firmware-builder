dc969baafc06faaaa45d78d77e7c8cbbbb6ea874
xiao-k233 <3115216193@qq.com>
2025-03-21 14:32:36 +0800

    fix:1v cn mib in luci

--- files/common/usr/sbin/8311-pingd.sh
+++ files/common/usr/sbin/8311-pingd.sh
@@ -7,9 +7,21 @@ while true ; do
 	# Ping host to help management work
 	PING_HOST=$(get_8311_ping_host)
 
-	echo "Starting ping to: $PING_HOST" | to_console
+	
 
 	if [ "$PINGD_ENABLED" -ne "0" ] 2>/dev/null; then
+		echo "Starting ping to: $PING_HOST" | to_console
+		ping -i 1 -c 3 "$PING_HOST" &> /dev/null < /dev/null
+		# 获取返回值（0=成功，非0=失败）
+		ping_result=$?
+		if [ "$ping_result" -eq 1 ] && [ "$restart_Num" -le 5 ]; then
+			echo "100% packet loss detected,try to restart lct port" | to_console
+			ifup "lct"
+			[ "$LCT_VLAN" -gt 0 ] && ifup "mgmt"
+			restart_Num=$((restart_Num+1))
+			sleep 5
+		fi
+
 		ping -i 5 "$PING_HOST" &> /dev/null < /dev/null
 		sleep 5
 	else
