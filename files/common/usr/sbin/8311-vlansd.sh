f543976754457e0790fee6980dbc436e428490b2
xiao-k233 <3115216193@qq.com>
2025-04-18 14:48:03 +0800

    feat: vlansd:detect config change

--- files/common/usr/sbin/8311-vlansd.sh
+++ files/common/usr/sbin/8311-vlansd.sh
@@ -8,6 +8,19 @@ pon_hash() {
         brctl show            # 显示网桥配置
     } | sha256sum | awk '{print $1}'
 }
+config_hash() {
+	{
+		fwenv_get_8311 "us_vlan_id"
+		fwenv_get_8311 "us_mc_vlan_id"
+		fwenv_get_8311 "ds_mc_tci"
+		fwenv_get_8311 "vlan_tag_ops"
+		fwenv_get_8311 "igmp_version"
+		fwenv_get_8311 "vlan_svc_log"
+		fwenv_get_8311 "force_us_vlan_id"
+		fwenv_get_8311 "force_me_create"
+		fwenv_get_8311 "force_me309_create"
+	} | sha256sum | awk '{print $1}'
+}
 
 FIX_ENABLED=$(fwenv_get_8311 "fix_vlans" "1")
 [ "$FIX_ENABLED" -eq 0 ] 2>/dev/null && exit 0
@@ -18,6 +31,7 @@ FIXES=""
 HOOKCMD=". /lib/8311-vlans-lib.sh && . $HOOK"
 
 LAST_HASH=""
+LAST_CFG_HASH=""
 
 echo "8311 VLANs daemon: start monitoring" | to_console
 while true ; do
@@ -25,12 +39,12 @@ while true ; do
 
 	if [ -n "$CMD" ] && [ -d "/sys/devices/virtual/net/gem-omci" ]; then
 		HASH=$(pon_hash)
-		if [ "$HASH" != "$LAST_HASH" ]; then
-			if VLANS=$(flock /tmp/8311-fix-vlans.lock -c "$CMD" 2>&1); then
-				LAST_HASH="$HASH"
-				echo "8311 VLANs daemon: new configuration detected, ran fix-vlans script:" | to_console
-				echo "$VLANS" | to_console
-			fi
+		CFG_HASH=$(config_hash)
+		if [ "$HASH" != "$LAST_HASH" ] || [ "$CFG_HASH" != "$LAST_CFG_HASH" ]; then
+			echo "8311 VLANs daemon: new configuration detected, ran fix-vlans script." | to_console
+			flock /tmp/8311-fix-vlans.lock -c "$CMD" 2>&1
+			LAST_HASH="$HASH"
+			LAST_CFG_HASH="$CFG_HASH"
 		fi
 	fi
 
